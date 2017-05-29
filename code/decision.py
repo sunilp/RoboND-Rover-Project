36ef3ecadfc436f477fc5fbcc77c14bc36754eb7
Sunil Prakash <prakashsunil@gmail.com>
2017-05-30 04:29:22 +0800

    updated

--- code/decision.py
+++ code/decision.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+
 # This is where you can build a decision tree for determining throttle, brake and steer 
 # commands based on the output of the perception_step() function
 def decision_step(Rover):
@@ -13,9 +14,41 @@ def decision_step(Rover):
     # Check if we have vision data to make decisions with
     if Rover.nav_angles is not None:
         # Check for Rover.mode status
+        
+        #if Rover.picking_up :
+         #   Rover.picking_up = 1
+        
         if Rover.mode == 'forward': 
             # Check the extent of navigable terrain
-            if len(Rover.nav_angles) >= Rover.stop_forward:  
+            if Rover.rock_angles is not None and len(Rover.rock_angles) > 0 :
+                Rover.steer = np.clip(np.mean(Rover.rock_angles * 180/np.pi), -15, 15)
+                #print('rock angle ' + Rover.steer)
+                #Rover.throttle = Rover.throttle-1
+                #Rover.brake = Rover.brake_set
+                if Rover.near_sample:
+                    Rover.mode == 'pickup'
+                    Rover.brake = Rover.brake_set
+                    Rover.send_pickup = True
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                    Rover.pick_up = False
+                    #Rover.picking_up = 0
+                    #Rover.mode == 'stop'
+                    Rover.brake = 0
+                    Rover.throttle = Rover.throttle_set
+                if Rover.vel > 1:
+                    #Rover.throttle = 0
+                    Rover.brake = Rover.brake_set
+                else:
+                    Rover.throttle = 1
+                    Rover.brake = 0
+                    Rover.mode == 'forward'
+                    #Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                    
+             
+           # if Rover.obstacles_angles is not None and len(Rover.obstacles_angles) > 0 :
+                #Rover.steer = np.clip(((np.mean(Rover.nav_angles)+np.mean(Rover.obstacles_angles)) * 180/np.pi), -15, 15)
+                #Rover.mode = 'pickup'
+            elif len(Rover.nav_angles) >= Rover.stop_forward:  
                 # If mode is forward, navigable terrain looks good 
                 # and velocity is below max, then throttle 
                 if Rover.vel < Rover.max_vel:
@@ -25,7 +58,7 @@ def decision_step(Rover):
                     Rover.throttle = 0
                 Rover.brake = 0
                 # Set steering to average angle clipped to the range +/- 15
-                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -20, 20)
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
             elif len(Rover.nav_angles) < Rover.stop_forward:
                     # Set mode to "stop" and hit the brakes!
@@ -38,6 +71,8 @@ def decision_step(Rover):
         # If we're already in "stop" mode then make different decisions
         elif Rover.mode == 'stop':
             # If we're in stop mode but still moving keep braking
+            #if Rover.near_sample== False:
+             #   Rover.mode = 'forward'
             if Rover.vel > 0.2:
                 Rover.throttle = 0
                 Rover.brake = Rover.brake_set
@@ -58,8 +93,20 @@ def decision_step(Rover):
                     # Release the brake
                     Rover.brake = 0
                     # Set steer to mean angle
-                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -17, 21)
                     Rover.mode = 'forward'
+                    
+        elif Rover.mode == 'pickup':
+            Rover.steer = np.clip(np.mean(Rover.rock_angles * 180/np.pi), -15, 15)
+            if Rover.near_sample:
+                #Rover.send_pickup = True
+                Rover.pick_up = False
+                Rover.throttle = Rover.throttle_set
+                Rover.mode == 'forward'
+                #Rover.picking_up = 0
+            else:
+                Rover.mode = 'forward'
+                Rover.throttle = Rover.throttle_set
     # Just to make the rover do something 
     # even if no modifications have been made to the code
     else:
--- code/perception.py
+++ code/perception.py
@@ -17,6 +17,26 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
     # Return the binary image
     return color_select
 
+#inverting the color for obstracles
+def obstacles(img, rgb_thresh=(70, 70, 70)):
+    obstacles = np.zeros_like(img[:,:,0])
+    below_thresh = (img[:,:,0] < rgb_thresh[0]) \
+                & (img[:,:,1] < rgb_thresh[1]) \
+                & (img[:,:,2] < rgb_thresh[2])
+    obstacles[below_thresh] = 1
+    return obstacles
+
+#imposing a lower and upper boundary in your color selection to be more specific about choosing colors
+def rocks(img):
+    low_yellow = np.array([20, 100, 100], dtype = "uint8")
+    high_yellow = np.array([255, 255, 255], dtype = "uint8")
+    
+    # convert to HSV space for better detection of yellow color
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV,3)
+    # mask yellow values
+    mask_rock = cv2.inRange(img_hsv, low_yellow, high_yellow)
+    return mask_rock
+
 # Define a function to convert to rover-centric coordinates
 def rover_coords(binary_img):
     # Identify nonzero pixels
@@ -38,13 +58,17 @@ def to_polar_coords(x_pixel, y_pixel):
     angles = np.arctan2(y_pixel, x_pixel)
     return dist, angles
 
-# Define a function to apply a rotation to pixel positions
 def rotate_pix(xpix, ypix, yaw):
     # TODO:
     # Convert yaw to radians
     # Apply a rotation
-    xpix_rotated = 0
-    ypix_rotated = 0
+    yaw_rad = yaw * np.pi / 180
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
+                            
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
+    
+    #xpix_rotated = 0
+    #ypix_rotated = 0
     # Return the result  
     return xpix_rotated, ypix_rotated
 
@@ -52,8 +76,10 @@ def rotate_pix(xpix, ypix, yaw):
 def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
     # TODO:
     # Apply a scaling and a translation
-    xpix_translated = 0
-    ypix_translated = 0
+    #xpix_translated = 0
+    #ypix_translated = 0
+    xpix_translated = (xpix_rot / scale) + xpos
+    ypix_translated = (ypix_rot / scale) + ypos
     # Return the result  
     return xpix_translated, ypix_translated
 
@@ -85,26 +111,77 @@ def perception_step(Rover):
     # TODO: 
     # NOTE: camera image is coming to you in Rover.img
     # 1) Define source and destination points for perspective transform
+    dst_size = 5 
+    bottom_offset = 6
+    source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+    destination = np.float32([[Rover.img.shape[1]/2 - dst_size, Rover.img.shape[0] - bottom_offset],
+                  [Rover.img.shape[1]/2 + dst_size, Rover.img.shape[0] - bottom_offset],
+                  [Rover.img.shape[1]/2 + dst_size, Rover.img.shape[0] - 2*dst_size - bottom_offset], 
+                  [Rover.img.shape[1]/2 - dst_size, Rover.img.shape[0] - 2*dst_size - bottom_offset],
+                  ])
+    
     # 2) Apply perspective transform
+    warped = perspect_transform(Rover.img, source, destination)
     # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
+    
+    colorsel_terrain = color_thresh(warped, rgb_thresh=(160, 160, 160))
+    threshed_obstacles = obstacles(warped)
+    threshed_rocks = rocks(warped)
+    
     # 4) Update Rover.vision_image (this will be displayed on left side of screen)
         # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
         #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
         #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
+        
+    Rover.vision_image[:,:,0] = threshed_obstacles * 255
+    Rover.vision_image[:,:,1] = threshed_rocks * 255
+    Rover.vision_image[:,:,2] = colorsel_terrain * 255    
 
     # 5) Convert map image pixel values to rover-centric coords
+    
+    terrain_xpix, terrain_ypix = rover_coords(colorsel_terrain)
+    obstacles_xpix, obstacles_ypix = rover_coords(threshed_obstacles)
+    rocks_xpix, rocks_ypix = rover_coords(threshed_rocks)
+    
     # 6) Convert rover-centric pixel values to world coordinates
+    
+    rover_xpos, rover_ypos = Rover.pos
+    scale = 10
+    navigable_x_world, navigable_y_world = pix_to_world(terrain_xpix, terrain_ypix, 
+                                    rover_xpos, rover_ypos,Rover.yaw, 
+                                    Rover.worldmap.shape[0], scale)
+    
+    obstacle_x_world, obstacle_y_world = pix_to_world(obstacles_xpix, obstacles_ypix,
+                                                        rover_xpos, rover_ypos,Rover.yaw, 
+                                                        Rover.worldmap.shape[0], scale)
+    
+    rock_x_world, rock_y_world = pix_to_world(rocks_xpix, rocks_ypix, 
+                                              rover_xpos, rover_ypos,Rover.yaw, 
+                                              Rover.worldmap.shape[0], scale) 
+    
     # 7) Update Rover worldmap (to be displayed on right side of screen)
         # Example: Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
         #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
         #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+        
+    Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
+    Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
+    Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1    
 
     # 8) Convert rover-centric pixel positions to polar coordinates
     # Update Rover pixel distances and angles
         # Rover.nav_dists = rover_centric_pixel_distances
         # Rover.nav_angles = rover_centric_angles
-    
+    rover_centric_pixel_distances, rover_centric_angles = to_polar_coords(terrain_xpix, terrain_ypix)
+    rover_centric_rock_dist, rover_centric_rock_angles = to_polar_coords(rocks_xpix, rocks_ypix)
+    rover_centric_obstacles_dist, rover_centric_obstacles_angles = to_polar_coords(obstacles_xpix, obstacles_ypix)
  
+    Rover.nav_dists = rover_centric_pixel_distances
+    Rover.nav_angles = rover_centric_angles
+    Rover.rock_dist = rover_centric_rock_dist
+    Rover.rock_angles = rover_centric_rock_angles
+    Rover.obstacles_dist = rover_centric_obstacles_dist
+    Rover.obstacles_angles = rover_centric_obstacles_angles
     
     
     return Rover
